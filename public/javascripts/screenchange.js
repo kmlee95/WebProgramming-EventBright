c50fee98c1b51b566b982297286ee895d1cbf1a0
kmlee95 <kmlee95@naver.com>
2017-11-26 22:23:09 +0900

    design change

--- public/javascripts/screenchange.js
+++ public/javascripts/screenchange.js
@@ -9,7 +9,15 @@ $(function(){
     $('.buttongroup .secondbutton').click(function(){
         $('.credit-edit2').hide();
         $('.credit-edit4').show();
-    }); 
+    });
+    $('.buttonback').click(function(){
+        $('.credit-edit3').hide();
+        $('.credit-edit4').hide();
+        $('.credit-edit2').show();
+    }) 
 });
 
-
+$(function(){
+    $('.question .title').css("color", "#"+(parseInt(Math.random()*0xffffff)).toString(16));
+    $('.eventname').css("color", "#"+(parseInt(Math.random()*0xffffff)).toString(16));
+});
